3abb4951d3c20a312883e9f8ec6b04de2affc7b1
Lusmaysh <89325163+Lusmaysh@users.noreply.github.com>
2021-08-25 19:44:48 +0700

    Update and rename Required.sh to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,3 @@
-clear
-pkg update && pkg upgrade -y
-termux-setup-storage
 pkg install python -y
 pip2 install youtube-dl -y
 pkg install ffmpeg -y
@@ -8,4 +5,3 @@ mkdir ~/.config/youtube-dl
 mkdir ~/storage/shared/Youtube
 mkdir ~/bin
 echo 'youtube-dl $1' > ~/bin/termux-url-opener
-python ydl.py
