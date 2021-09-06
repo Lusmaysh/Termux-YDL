8b9237889703e380d870c9b6ff9ff46e181f391f
Lusmaysh <89325163+Lusmaysh@users.noreply.github.com>
2021-09-06 20:56:41 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,2 @@
-pkg install ffmpeg -y
-pip install youtube-dl -y
-mkdir ~/.config/youtube-dl
-mkdir ~/storage/shared/Youtube
-mkdir ~/bin
-echo 'youtube-dl $1' > ~/bin/termux-url-opener
-echo '--no-mtime -o /data/data/com.termux/files/home/storage/shared/Youtube/%(title)s.%(ext)s -f "mp4[height<=2160]"' > ~/.config/youtube-dl/config
+pkg install ffmpeg && pip install youtube-dl
+mkdir ~/.config/youtube-dl;mkdir ~/storage/shared/Youtube;mkdir ~/bin;echo 'youtube-dl $1' > ~/bin/termux-url-opener;echo '--no-mtime -o /data/data/com.termux/files/home/storage/shared/Youtube/%(title)s.%(ext)s -f "mp4[height<=2160]"' > ~/.config/youtube-dl/config;
